49a68ea11c831b69e18b41949bad1622ddcf11e5
Lim-Ji-Hyeon <sen03620@naver.com>
2021-04-13 23:37:20 +0900

    first

--- app/src/main/java/com/example/environmentalcampaign/FeedPage.java
+++ app/src/main/java/com/example/environmentalcampaign/FeedPage.java
@@ -1,21 +1,45 @@
 package com.example.environmentalcampaign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeedPage extends AppCompatActivity {
 
     TextView tv_home, tv_make, tv_certi, tv_feed, tv_mypage;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_page);
+
+        // 피드에 recyclerView 삽입
+
+        RecyclerView feedRecyclerView = findViewById(R.id.feedRecyclerView);
+        feedRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        // 이미지 넣음
+        List<FeedItem> feedItems = new ArrayList<>();
+        feedItems.add(new FeedItem(R.drawable.cp1));
+        feedItems.add(new FeedItem(R.drawable.cp2));
+        feedItems.add(new FeedItem(R.drawable.cp3));
+        feedItems.add(new FeedItem(R.drawable.cp4));
+        feedItems.add(new FeedItem(R.drawable.cp5));
+        feedItems.add(new FeedItem(R.drawable.cp6));
+
+        feedRecyclerView.setAdapter(new FeedAdapter(feedItems));
+
+
+
 
         // 하단 메뉴바 페이지 연동
 
