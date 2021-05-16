7dd553a037ec2b32b8cdfa98cd8472a8f449289a
Lim-Ji-Hyeon <sen03620@naver.com>
2021-05-17 00:03:52 +0900

    camera

---
+++ app/src/main/java/com/example/environmentalcampaign/feed/FeedImageDetailPage.java
@@ -0,0 +1,16 @@
+package com.example.environmentalcampaign.feed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.environmentalcampaign.R;
+
+public class FeedImageDetailPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_image_detail_page);
+    }
+}
